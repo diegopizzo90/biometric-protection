e6dc653fe55827ef24f3cc33b8b5075f26f6ca75
Diego Pizzo <>
2022-02-10 22:38:33 +0000

    remove koin and add hilt

--- app/src/main/java/com/diegopizzo/biometricprotection/Application.kt
+++ app/src/main/java/com/diegopizzo/biometricprotection/Application.kt
@@ -1,18 +1,10 @@
 package com.diegopizzo.biometricprotection
 
 import android.app.Application
-import com.diegopizzo.biometricprotection.config.viewModelModule
-import org.koin.android.ext.koin.androidContext
-import org.koin.core.context.startKoin
 
 class Application : Application() {
 
     override fun onCreate() {
         super.onCreate()
-
-        startKoin {
-            androidContext(this@Application)
-            modules(listOf(viewModelModule))
-        }
     }
 }
